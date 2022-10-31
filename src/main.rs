3c1c7aa96e3c06bc4c02fc0812035577c19a9c27
Bryan Clement <bclement01@gmail.com>
2022-10-31 16:40:06 -0700

    things terminate, need to organize things

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,55 @@
-use std::{ops::{Add, Mul, Sub, Index, IndexMut}, slice::SliceIndex};
+use std::{ops::{SubAssign, AddAssign, Add, Mul, Sub, Index, IndexMut}, slice::SliceIndex};
+use std::fmt;
+
+
+// TODO: come up with a better name, it isn't really a spatial vector just had to avoid name collision
 #[derive(Debug, Copy, Clone, PartialEq)]
 struct SpatialVec<const SIZE: usize>([f64; SIZE]);
+impl<const SIZE: usize> SpatialVec<{SIZE}> {
+    fn dot(self: Self, other: Self) -> f64 {
+        let mut res = 0.0;
+        for i in 0..SIZE {
+            res += self[i] * other[i];
+        }
+        res
+    }
+
+    fn norm(self: &Self) -> f64 {
+        let SpatialVec(data) = self;
+        let mut res: f64 = 0.0;
+        for d in data {
+            res += d*d;
+        }
+        res.powf(0.5)
+    }
+}
+
+impl<const SIZE: usize> fmt::Display for SpatialVec<{SIZE}> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(");
+        for i in 0..SIZE-1 {
+            write!(f, "{}, ", self[i]);
+        }
+        write!(f, "{})", self[SIZE - 1])
+    }
+}
+
+impl<const SIZE: usize> SubAssign for SpatialVec<{SIZE}> {
+    fn sub_assign(&mut self, other: Self) {
+        for i in 0..SIZE {
+            self[i] = self[i] - other[i];
+        }
+    }
+}
+
+impl<const SIZE: usize> AddAssign for SpatialVec<{SIZE}> {
+    fn add_assign(&mut self, other: Self) {
+        for i in 0..SIZE {
+            self[i] = self[i] + other[i];
+        }
+    }
+}
+
 impl<const SIZE: usize> From<[f64; SIZE]> for SpatialVec<{SIZE}>
 {
     fn from(x: [f64; SIZE]) -> Self {
@@ -74,51 +123,83 @@ where
         &mut data[i]
     }
 }
+#[derive(Clone, Copy)]
+struct DescentPredicateData
+{
+    steps: usize,
+    grad: SpatialVec<2>,
+    position: SpatialVec<2>,
+    value: f64
+}
 
 // TODO: break this into its own file with some tests
-struct Optimizer
+// TODO: make the dimension generic
+struct Optimizer<'a>
 {
     initial_step_length: f64,
     step_contraction_factor: f64,
-    step_threshold_coefficient: f64
+    step_threshold_coefficient: f64,
+    objective: &'a dyn Fn(SpatialVec<2>) -> f64,
+    derivs: RosenDerivatives
 }
 
-impl Optimizer 
+impl Optimizer<'_>
 {
-    fn new(
-        initial_step_length: f64,
-        step_contraction_factor: f64,
-        step_threshold_coefficient: f64
-    ) -> Self {
-        Self { initial_step_length, step_contraction_factor, step_threshold_coefficient }
-    }
+    fn calc_next_step_length(self: &Self, start:SpatialVec<2>, step_direction: SpatialVec<2>) -> f64 {
+        let objective = self.objective;
+        let start_value = objective(start);
+        let grad = self.derivs.gen_grad(start);
+        let candidate_dot = self.step_threshold_coefficient * step_direction.dot(grad);
 
-    fn calc_next_step_length(self: &Optimizer, start:SpatialVec<2>, step_direction: SpatialVec<2>) -> f64 {
-        start + step_direction;
-        0.0
-    }
+        let mut step_size = self.initial_step_length;
 
-    fn descent(self: &Optimizer, start: SpatialVec<2>, step_length: f64, num_steps: usize, direction_gen: &dyn Fn(SpatialVec<2>) -> SpatialVec<2>) -> Vec<SpatialVec<2>> {
-        let mut res: Vec<SpatialVec<2>> = vec![SpatialVec([0.0, 0.0]); num_steps + 1];
-        let mut curr_step = start;
-        for i in 0..=num_steps {
-            res[i] = curr_step;
-            let step_direction = direction_gen(curr_step);
-            curr_step = curr_step - (step_length * step_direction);
+        while objective(start + step_size * step_direction) > start_value + step_size * candidate_dot {
+            step_size *= self.step_contraction_factor;
         }
+
+        step_size
+    }
+
+    fn descent(self: &Self, start: SpatialVec<2>, should_step: &dyn Fn(DescentPredicateData) -> bool, direction_gen: &dyn Fn(SpatialVec<2>) -> SpatialVec<2>) -> Vec<SpatialVec<2>> {
+        let objective = self.objective;
+
+        let mut res: Vec<SpatialVec<2>> = vec![];
+        let mut curr_step = start;
+        let mut pred_data = DescentPredicateData {
+            grad: self.derivs.gen_grad(curr_step),
+            steps: 0,
+            position: curr_step,
+            value: objective(curr_step),
+        };
+
+        while should_step(pred_data) {
+            res.push(SpatialVec::clone(&curr_step));
+
+            let step_direction = direction_gen(curr_step);
+            let step_length = Optimizer::calc_next_step_length(self, curr_step, step_direction);
+
+            curr_step += (step_length * step_direction);
+
+            pred_data.grad = self.derivs.gen_grad(curr_step);
+            pred_data.steps += 1;
+            pred_data.position = curr_step;
+            pred_data.value = objective(curr_step);
+        }
+        res.push(curr_step);
         res
     }
 }
 
 // TODO: break this into its own file with some tests
-#[derive(Debug)]
-struct Derivatives
+// TODO: make this into a generic interface
+#[derive(Debug, Clone)]
+struct RosenDerivatives
 {
     rosen_coefficient: f64
 }
 
 // TODO: cache the values per struct
-impl Derivatives
+impl RosenDerivatives
 {
     fn new(rosen_coefficient: f64) -> Self
     {
@@ -126,7 +207,7 @@ impl Derivatives
     }
 
     // TODO: implement numerical grad method
-    fn gen_grad(self: &Derivatives, x: SpatialVec<2>) -> SpatialVec<2>
+    fn gen_grad(self: &RosenDerivatives, x: SpatialVec<2>) -> SpatialVec<2>
     {
         SpatialVec([
             self.rosen_coefficient*-4.0*x[0] * (x[1] - x[0].powi(2)) - 2.0*(1.0  - x[0]),
@@ -134,14 +215,17 @@ impl Derivatives
         ])
     }
 
-    fn gen_newton(self: &Derivatives, x: SpatialVec<2>) -> SpatialVec<2>
+    fn gen_newton(self: &RosenDerivatives, x: SpatialVec<2>) -> SpatialVec<2>
     {
-        let grad = Derivatives::gen_grad(self, x);
+        let grad = RosenDerivatives::gen_grad(self, x);
+
         let a = self.rosen_coefficient*-4.0 * x[1] + 1200.0 * x[0].powi(2) + 2.0;
         let b = self.rosen_coefficient*-4.0 * x[0];
         let c = b;
         let d = self.rosen_coefficient*2.0;
+
         let scale = 1.0/(a*d - b*c);
+
         SpatialVec([
             scale * (d*grad[0] - b*grad[1]),
             scale * (a*grad[1] - c*grad[0]),
@@ -161,37 +245,54 @@ fn gen_latex_table(steps: Vec<SpatialVec<2>>, func: &dyn Fn(SpatialVec<2>) -> f6
     }
 }
 
+fn gen_table(steps: Vec<SpatialVec<2>>, func: &dyn Fn(SpatialVec<2>) -> f64)
+{ 
+    for i in 0..steps.len() {
+        let step = steps.get(i);
+        match step {
+            Some(step) => println!("step {}: f({:?}, {:?}) = {:?}", i, step[0], step[1], func(*step)),
+            None => println!("oops"),
+        }
+    }
+}
+
 fn main()
 {
-    let optimizer = &Optimizer::new(
-        1.0,
-        0.5,
-        0.01
-    );
-    let derivs = &Derivatives::new(100.0);
-    let func = |x: SpatialVec<2>| 100.0 * (x[1] - x[0].powi(2)).powi(2) + (1.0 - x[0]).powi(2);
+    let rosen_coefficient = 1.0;
+    let func = |x: SpatialVec<2>| rosen_coefficient * (x[1] - x[0].powi(2)).powi(2) + (1.0 - x[0]).powi(2);
+    let derivs = RosenDerivatives::new(rosen_coefficient);
+    let optimizer = &Optimizer {
+        initial_step_length: 1.0,
+        step_contraction_factor: 0.5,
+        step_threshold_coefficient: 0.01,
+        objective: &func,
+        derivs: RosenDerivatives::clone(&derivs)
+    };
+
+    let step_predicate = |data: DescentPredicateData| data.steps < 5;
+    let grad_predicate = |data: DescentPredicateData| data.grad.norm() > 1.0e-3;
 
     println!("GRAD DESCENT");
-    gen_latex_table(
+    gen_table(
         Optimizer::descent(
             optimizer,
-            SpatialVec([0.0,0.0]),
-            1.0,
-            5,
-            &|x: SpatialVec<2>| Derivatives::gen_grad(derivs, x),
+            SpatialVec([-1.2, 1.0]),
+            //&step_predicate,
+            &grad_predicate,
+            &|x: SpatialVec<2>| -1.0*RosenDerivatives::gen_grad(&derivs, x),
         ),
         &func,
     );
 
     println!("\n\n");
     println!("NEWTON ITERATION");
-    gen_latex_table(
+    gen_table(
         Optimizer::descent(
             optimizer,
-            SpatialVec([0.0,0.0]),
-            1.0,
-            5,
-            &|x: SpatialVec<2>| Derivatives::gen_newton(derivs, x),
+            SpatialVec([-1.2, 1.0]),
+            //&step_predicate,
+            &grad_predicate,
+            &|x: SpatialVec<2>| -1.0*RosenDerivatives::gen_newton(&derivs, x),
         ),
         &func,
     );
